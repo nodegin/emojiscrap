ca077594b7cde6bc757f9684e95ac27018a4bb28
nodegin <10@nodeg.in>
2018-10-31 22:31:06 +0800

    fix

--- compress.sh
+++ compress.sh
@@ -7,6 +7,9 @@ if [[ $# -eq 0 ]] ; then
   exit 1
 fi
 
+# Abort on API Key error
+set -e
+
 for file in ./validated/*.png; do
   downloadLink=`curl --user api:$1 --data-binary @$file -s https://api.tinify.com/shrink | python -c "import sys, json; print json.load(sys.stdin)['output']['url']; sys.exit(0)"`
   echo "Downloading $file ($downloadLink)"
